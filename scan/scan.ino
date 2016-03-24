7ed650d62dd8f06d62dae77f8c57f7d0c01b1f65
Jussi Salin <salinjus@gmail.com>
2016-03-25 00:10:25 +0200

    Some fixes to rotation calculation

--- scan/scan.ino
+++ scan/scan.ino
@@ -22,14 +22,17 @@
 #define DIR_PIN A2             // pin for direction signal
 #define STEP_PIN A0            // pin for step signal
 #define IR_PIN 2               // pin for IR LED
-#define PHOTOS_TO_TAKE 60      // number of photos or frames to take during a full rotation
-#define PRE_PHOTO_DELAY 1000   // ms to sleep before taking a photo (object stabilizes)
+#define PHOTOS_TO_TAKE 180     // number of photos or frames to take during a full rotation
+#define PRE_PHOTO_DELAY 3000   // ms to sleep before taking a photo (object stabilizes)
 #define POST_PHOTO_DELAY 1000  // ms to sleep after taking a photo (camera lag)
 #define STEP_DELAY 10          // ms to sleep between steps (to rotate at good speed)
 #define STARTUP_DELAY 3000     // ms to sleep at the beginning
                                // (it might take time for user to remove hands after power up)
 #define LED_PIN 13             // On while taking a photo, off when rotating
 #define DUMMY_PHOTOS 3         // How many "dummy" photos in the beginning before rotation
+#define TURN_AMOUNT 450        // How many degrees to rotate while taking the photos
+                               // (add additional "tolerance" degrees if motor is not accurate)
+#define STEP_DT float
 
 void setup()
 {
@@ -51,6 +54,22 @@ void loop()
 {
   Canon Camera(IR_PIN);
 
+  // Calculate how many steps to take when turning per photo
+  // Divides by 2 because step is taken on both rising and lowering edge of pulse
+  STEP_DT cycles_per_360 = STEPS_FOR_360 / 2;
+  STEP_DT multiplier = TURN_AMOUNT / 360;
+  STEP_DT photos_to_take = PHOTOS_TO_TAKE;
+  Serial.print("cycles_per_360=");
+  Serial.println(cycles_per_360);
+  Serial.print("multiplier=");
+  Serial.println(multiplier);
+  Serial.print("photos_to_take=");
+  Serial.println(photos_to_take);
+  STEP_DT steps_to_take = cycles_per_360 * multiplier / photos_to_take;
+  Serial.print("Doing ");
+  Serial.print(steps_to_take);
+  Serial.println(" steps per photo");
+  
   delay(STARTUP_DELAY);
   Serial.println("Taking few photos to calibrate camera");
   for (int i=0; i<DUMMY_PHOTOS; i++)
@@ -69,23 +88,19 @@ void loop()
     digitalWrite(LED_PIN, HIGH);
     Serial.print("Taking photo #");
     Serial.println(i);
-    // Take photo
     delay(PRE_PHOTO_DELAY);
     Camera.shutterNow();
     delay(POST_PHOTO_DELAY);
     digitalWrite(LED_PIN, LOW);
 
     // Rotate
-    unsigned long steps_to_take = STEPS_FOR_360/2/PHOTOS_TO_TAKE;
-    for (unsigned long j=0; j<steps_to_take; j++)
+    for (STEP_DT j=0; j<steps_to_take; j++)
     {
-      //Serial.print(".");
       digitalWrite(STEP_PIN, LOW);
       delay(STEP_DELAY);
       digitalWrite(STEP_PIN, HIGH);
       delay(STEP_DELAY);
     }
-    //Serial.println();
   }
 
   Serial.println("Done!");
